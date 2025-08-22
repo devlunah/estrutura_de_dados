47784991daf8d69c8d9201e3e1e48d7c3690ca33
devlunah <lunah.silvac@gmail.com>
2025-08-22 10:35:16 -0500

    questao 20

--- trabalho01/q20.cpp
+++ trabalho01/q20.cpp
@@ -3,6 +3,14 @@
 int main(){
     // Multiplique cada valor do vetor por sua posição
 
-    
+    int numeros [7] = {-43, 18, -7, 92, -12, 5, -66};
+    int tamanhoArray = sizeof(numeros)/sizeof(numeros[0]);
 
+    for (int i = 0; i < tamanhoArray; i++){
+        numeros[i] = numeros[i] * i;
+    } 
+
+    for (int b = 0; b < tamanhoArray; b++){
+        printf("%d \n", numeros[b]);
+    }
 }
