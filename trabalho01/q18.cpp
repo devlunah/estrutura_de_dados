3724dddb95f4f5c3e50564b434e5954a01cc4e55
devlunah <lunah.silvac@gmail.com>
2025-08-22 10:30:54 -0500

    questao 18

--- trabalho01/q18.cpp
+++ trabalho01/q18.cpp
@@ -3,6 +3,17 @@
 int main(){
     // Substitua os valores negativos por zero
 
-    
+    int numeros [15] = {-63, 27, -11, 92, -5, 48, -36, 7, -89, 54, -22, 13, -74, 31, -2};
+    int tamanhoArray = sizeof(numeros)/sizeof(numeros[0]);
 
+    for (int i = 0; i < tamanhoArray; i++){
+
+        if(numeros[i] < 0){
+            numeros[i] = 0;
+        }
+    }
+
+    for (int b = 0; b < tamanhoArray; b++){
+        printf("%d \n", numeros[b]);
+    }
 }
