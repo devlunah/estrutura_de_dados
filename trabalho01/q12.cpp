5abb253491497c11a1bdf1087c37810d0f18ff43
devlunah <lunah.silvac@gmail.com>
2025-08-22 14:10:59 -0500

    questao 19

--- trabalho01/q12.cpp
+++ trabalho01/q12.cpp
@@ -3,6 +3,29 @@
 int main(){
     // Elimine valores repetidos de um vetor de 10 elementos
 
+    int numeros [] = {7, 12, 7, 3, 18, 12, 25, 25, 4, 18};
+    int tamanhoArray = sizeof(numeros)/sizeof(numeros[0]);
+    int indice_remocao;
+
     
 
+    // for (int contador = 1; contador < tamanhoArray; contador++){
+    //     for (int i = 0; i < tamanhoArray; i++) {
+    //         if (numeros[i] == numeros[i + 1]) {
+    //             indice_remocao = numeros[i];
+
+    //             for (int b = indice_remocao; b < tamanhoArray - 1; b++){
+    //                     numeros[b] = numeros[b+1];
+    //             }
+
+    //         }
+    //     }
+    // }
+
+    
+    
+    // printf("\nArray sem elementos repetidos: \n");
+    // for (int i = 0; i < tamanhoArray; i++) {
+    //     printf("%d \n", numeros[i]);
+    // }
 }
--- trabalho01/q19.cpp
+++ trabalho01/q19.cpp
@@ -4,5 +4,33 @@ int main(){
     // Calcule a média dos valores pares de um vetor
 
     
+    int array_numeros [15] = {47, 83, 12, 5, 91, 42, 3, 67, 28, 19, 64, 7, 35, 58, 10};
+    int tamanhoArray = sizeof(array_numeros)/sizeof(array_numeros[0]);
+
+    int array_num_pares[15];
+    int count = 0;
+    int soma = 0;
+
+    // copiar o vetor
+    for(int i = 0; i < tamanhoArray; i++){
+        if(array_numeros[i] % 2 == 0){
+            array_num_pares[count] = array_numeros[i];
+            count++;
+        } 
+    }
+    // mostrar o vetor par
+    for (int b = 0; b < count; b++){
+        printf("Vetor copiado : %d \n", array_num_pares[b]);
+    }
+
+    // calcular média
+    for (int b = 0; b < count; b++){
+        soma += array_num_pares[b];
+    }
+
+    printf("%d \n", count);
+
+    float media = soma / count;
+    printf("%.2f \n", media);
 
 }
