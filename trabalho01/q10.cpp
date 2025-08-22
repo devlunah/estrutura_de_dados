40e77b5b305012fa56932f369a95169402f5d60f
devlunah <lunah.silvac@gmail.com>
2025-08-22 10:01:38 -0500

    questões 10 e 11

--- trabalho01/q10.cpp
+++ trabalho01/q10.cpp
@@ -1,33 +1,24 @@
 #include <stdio.h>
 
-void numeros(){
-    int array_numeros [15] = {47, 83, 12, 5, 91, 42, 3, 67, 28, 19, 64, 7, 35, 58, 10}; 
-}
+int main(){
 
-void numPares(int array_numeros[], int array_num_pares[]){
+    int array_numeros [15] = {47, 83, 12, 5, 91, 42, 3, 67, 28, 19, 64, 7, 35, 58, 10};
     int tamanhoArray = sizeof(array_numeros)/sizeof(array_numeros[0]);
 
+    int array_num_pares[15];
+    int count = 0;
+
+    // copiar o vetor
     for(int i = 0; i < tamanhoArray; i++){
-
         if(array_numeros[i] % 2 == 0){
-            array_num_pares[i] = array_numeros[i];
-        }
+
+            array_num_pares[count] = array_numeros[i];
+            count++;
+        } 
     }
-}
 
-int main(void){
-    // Copie os valores pares de um vetor para outro
-
-    int array_numeros[15];
-    int array_num_pares[1];
-
-    numeros();
-    numPares(array_numeros, array_num_pares);
-
-    printf("Vetor original: %d \n", array_numeros);
-
-    printf("\nVetor copiado : %d \n", array_num_pares);
-
-    return 0;
-
+    // mostrar o vetor par
+    for (int b = 0; b < count; b++){
+        printf("Vetor copiado : %d \n", array_num_pares[b]);
+    }
 }
--- trabalho01/q11.cpp
+++ trabalho01/q11.cpp
@@ -2,6 +2,24 @@
 
 int main(){
     // Ordene um vetor de 10 elementos em ordem crescente
+    // MÉTODO BUBBLE SORT
 
+    int numeros [10] = {-47, 23, -8, 91, -32, 5, -76, 14, -1, 60};
+    int tamanhoArray = sizeof(numeros)/sizeof(numeros[0]);
+    int ordenador;
 
+    for (int contador = 1; contador < tamanhoArray; contador++) {
+        for (int i = 0; i < tamanhoArray - 1; i++) {
+            if (numeros[i] > numeros[i + 1]) {
+                ordenador = numeros[i];
+                numeros[i] = numeros[i + 1];
+                numeros[i + 1] = ordenador;
+            }
+        }
+    }
+
+    printf("\nElementos do array em ordem crescente:\n");
+    for (int i = 0; i < tamanhoArray; i++) {
+        printf("%d \n", numeros[i]);
+    }
 }
